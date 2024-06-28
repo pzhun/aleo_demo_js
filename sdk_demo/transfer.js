69fea22872cbeae2e65afec558eb37df01fe3386
zhun <1414807496@qq.com>
2024-06-28 11:57:23 +0800

    remove private key.

--- sdk_demo/transfer.js
+++ sdk_demo/transfer.js
@@ -7,16 +7,12 @@ import {
 } from "@aleohq/sdk";
 
 const sender = {
-  address: "aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px",
-  path: `m/44'/0'/0'/0'`,
-  view_key: "AViewKey1mSnpFFC8Mj4fXbK5YiWgZ3mjiV8CxA79bYNa8ymUpTrw",
-  private_key: "APrivateKey1zkp8CZNn3yeCseEtxuVPbDCwSyhGW6yZKUYKfgXmcpoGPWH",
+  address: "aleo1xxxx",
+  private_key: "APrivateKey1xxxxx",
 };
 const receiver = {
-  address: "aleo127c79p7k4jj9e2c8kwwqsn5qkavun07etkyqpr795eyrdnyh3uzqnf8nfn",
-  path: `m/44'/0'/0'/0'`,
-  view_key: "AViewKey1tQY7eCFZhX6wxNDpuTeBoCQEn3KsmmwoY9rUBWhxBdjp",
-  private_key: "APrivateKey1zkpC2CbihCvUyg8zcNXTngzGpmCzKTF8uZP4jfyu3LdfT8v",
+  address: "aleo1xxxx",
+  private_key: "APrivateKey1xxxx",
 };
 
 // curl 23.20.9.85:3030/testnet/latest/height
@@ -46,22 +42,22 @@ async function transfer() {
   const address = account.address().to_string();
   
   console.log(await networkClient.getLatestBlock());
-  // console.log(
-  //   await networkClient.getProgramMappingValue(
-  //     "credits.aleo",
-  //     "account",
-  //     address
-  //   )
-  // );
+  console.log(
+    await networkClient.getProgramMappingValue(
+      "credits.aleo",
+      "account",
+      address
+    )
+  );
 
-  // /// 发送交易
-  // const tx_id = await programManager.transfer(
-  //   1,
-  //   received,
-  //   "transfer_private",
-  //   0.1
-  // );
-  // return tx_id;
+  /// 发送交易
+  const tx_id = await programManager.transfer(
+    1,
+    received,
+    "transfer_private",
+    0.1
+  );
+  return tx_id;
 }
 
 transfer();
